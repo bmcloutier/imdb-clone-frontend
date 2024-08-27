31f6f350963f696431f1d998772ce75b53b2a1b9
Brian Cloutier <bmcloutier@gmail.com>
2024-08-27 13:07:21 -0500

    Added index action

---
+++ src/components/MoviesIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    movies: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }} [{{ movie.year }}]</h2>
+      <img v-bind:src="movie.image" width="400" />
+      <h3>{{ movie.genre }}</h3>
+      <p>{{ movie.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
