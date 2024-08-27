351caf0db8baa3ebdd5f9df98ace7d7a59659794
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-08-27 12:22:10 -0700

    created a create action

---
+++ src/components/MoviesNew.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data: function () {
+    return {
+      newMovieParams: {},
+    };
+  },
+  methods: {
+    handleSubmit: function () {
+      this.$emit("createMovie", this.newMovieParams);
+      this.newMovieParams = {};
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>New Movie</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Name:
+        <input type="text" v-model="newMovieParams.url" />
+      </div>
+      <div>
+        image
+        <input type="text" v-model="newMovieParams.url" />
+      </div>
+      <div>
+        description
+        <input type="text" v-model="newMovieParams.url" />
+      </div>
+      <div>
+        year
+        <input type="number" v-model="newMovieParams.url" />
+      </div>
+      <div>
+        genre
+        <input type="text" v-model="newMovieParams.url" />
+      </div>
+      <div></div>
+      <button type="submit">Create Movie</button>
+    </form>
+  </div>
+</template>
+
+<style></style>
